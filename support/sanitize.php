661d7eccfc72923bf4191c6bac259e5db1a0b5a0
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-17 12:59:49 -0600

    No filter_var in 4.3

---
+++ support/sanitize.php
@@ -0,0 +1,13 @@
+<?php
+
+function sanitizeFilenamePart($input) {
+	static $unacceptable = "[^[:alnum:]-_.]";
+	return preg_filter($unacceptable, "_", $input);
+}
+
+function sanitizeLine($input) {
+	static $unacceptable = "[^[:alnum:][:punct:][:space:]]";
+	return str_replace(array("<?", "?>"), "", preg_filter($unacceptable, "_", $input));
+}
+
+?>
