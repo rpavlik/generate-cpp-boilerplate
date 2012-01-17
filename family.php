c9e63f6ae9352c6f03129966108b800aec2b640d
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-17 17:30:53 -0600

    more web work

--- family.php
+++ family.php
@@ -7,3 +7,12 @@ function getDownloadLink($ext) {
 	return '<a href="' . getDownloadURL($ext) . '">' . "Click to download the .$ext file</a>";
 }
 ?>
+<html>
+<head><title>C++ Boilerplate Generator - Download Page</title></head>
+<body>
+<ul>
+	<li><?php echo getDownloadLink("cpp"); ?></li>
+	<li><?php echo getDownloadLink("h"); ?></li>
+</ul>
+</body>
+</html>
