8a8490273ec9ddd591ff2318566cdf52b26dead4
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-17 12:26:44 -0600

    some include to requires

--- download.php
+++ download.php
@@ -2,6 +2,6 @@
 $ext = filter_var($_GET["ext"], FILTER_SANITIZE_STRING);
 $filebase = filter_var($_GET["filebase"], FILTER_SANITIZE_STRING);
 
-include("generate.php");
+require("generate.php");
 generateBoilerplate($ext, $filebase, $defaultAuthor, $defaultLicense);
 ?>
