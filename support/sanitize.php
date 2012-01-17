9170bf04969336dae780c7fbdfa89be45a117080
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-17 13:42:03 -0600

    use filter_var again

--- support/sanitize.php
+++ support/sanitize.php
@@ -1,13 +1,19 @@
 <?php
 
 function sanitizeFilenamePart($input) {
+	return filter_var($input, FILTER_SANITIZE_STRING);
+	/*
 	static $unacceptable = '[^[:alnum:]-_.]';
 	return preg_filter($unacceptable, '_', $input);
+	*/
 }
 
 function sanitizeLine($input) {
+	return filter_var($input, FILTER_SANITIZE_STRING);
+	/*
 	static $unacceptable = '[^[:alnum:][:punct:][:space:]]';
 	return str_replace(array('<?', '?>'), '', preg_filter($unacceptable, '_', $input));
+	*/
 }
 
 ?>
