796bbfdcf16746e923a38129051b61d691fc9586
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-17 13:49:42 -0600

    Extend the attachment script

--- support/attachment.php
+++ support/attachment.php
@@ -2,8 +2,10 @@
 
 function generateAttachment($filename, $mimetype)
 {
-	header('Content-disposition: attachment; filename=' . $filename);
+	header('Content-disposition: attachment; filename="' . $filename . '"');
 	header('Content-type: ' . $mimetype);
+	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 }
 
 ?>
