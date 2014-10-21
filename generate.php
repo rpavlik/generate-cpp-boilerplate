efd6cf33cfafd681af35f3c9a1557c5073b63363
Ryan Pavlik <ryan@sensics.com>
2014-10-21 08:22:36 -0500

    Tabs to spaces.

--- generate.php
+++ generate.php
@@ -12,6 +12,8 @@ require 'support/attachment.php';
 
 // TODO don't hardcode this author and license stuff
 
+$indentation = "    ";
+
 $defaultAuthor =
 	'Ryan Pavlik
 &lt;ryan@sensics.com&gt;
@@ -24,7 +26,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -43,7 +45,7 @@ date_default_timezone_set('America/New_York');
 function indentAuthorInfo($authorinfo) {
 	return implode("\n", array_map(
 			function ($line) {
-				return "\t" . trim($line);
+				return $indentation . trim($line);
 			},
 			explode("\n", $authorinfo)
 		)
