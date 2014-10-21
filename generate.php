c2a10ae26f983f8e77af250a1e5d11f53aea8256
Ryan Pavlik <ryan@sensics.com>
2014-10-21 08:10:40 -0500

    Update for sensics.

--- generate.php
+++ generate.php
@@ -14,18 +14,25 @@ require 'support/attachment.php';
 
 $defaultAuthor =
 	'Ryan Pavlik
-<rpavlik@iastate.edu> and <abiryan@ryand.net>
-http://academic.cleardefinition.com/
-Iowa State University Virtual Reality Applications Center
-Human-Computer Interaction Graduate Program';
+&lt;ryan@sensics.com&gt;
+&lt;http://sensics.com&gt;';
 
 $defaultLicense =
-	'          Copyright Iowa State University @YEAR@.
- Distributed under the Boost Software License, Version 1.0.
-    (See accompanying file LICENSE_1_0.txt or copy at
-          http://www.boost.org/LICENSE_1_0.txt)';
+	'Copyright @YEAR@ Sensics, Inc.
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.';
+
+date_default_timezone_set('America/New_York');
 
 /**
  *
